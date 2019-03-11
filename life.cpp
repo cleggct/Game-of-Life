7fb5a58bb4edd6c59d21cb93a3bac6a3054ad30b
unknown <cleggct@gmail.com>
2019-03-11 15:49:59 -0700

    defined the paintEvent function

--- life.cpp
+++ life.cpp
@@ -6,6 +6,98 @@ life::life(QWidget *parent) :
     ui(new Ui::life)
 {
     ui->setupUi(this);
+
+    setMinimumSize(max_sz * cell_sz, max_sz * cell_sz); //set the min and max sizes of the widget
+    setMaximumSize(max_sz * cell_sz, max_sz * cell_sz);
+    setSizeIncrement(cell_sz, cell_sz); //the size of the window will increment by cell sizes
+
+    randomize(); //randomize the cells
+
+    resize(max_sz * cell_sz, max_sz * cell_sz); //set the size of the widget
+}
+
+void life::clear(){
+    for(size_t i = 0; i < max_sz; ++i){ //iterate over all the cells
+        for(size_t j = 0; j < max_sz; ++j){
+            cells[i][j] = 0; //make each cell nonliving
+        }
+    }
+}
+
+void life::randomize(){
+    std::srand(std::time(0)); //seed the random number generator
+    for(size_t i = 0; i < max_sz; ++i){ //iterate over all the cells
+        for(size_t j = 0; j < max_sz; ++j){
+            cells[i][j] = std::rand() % 2; //set the cell to a random value
+        }
+    }
+}
+
+void life::nextGeneration(){
+    for(size_t i = 0; i < max_sz; ++i){ //iterate over all the cells
+        for(size_t j = 0; j < max_sz; ++j){
+            int total = 0; //initialize a total count of the living neighbors
+
+            //count the number of living neighbors
+            if(cells[(i-1+max_sz)%max_sz][(j-1+max_sz)%max_sz]){ //we use these indexes to make the world wrap around at the edges
+                ++total;
+            }
+            if(cells[(i-1+max_sz)%max_sz][(j+max_sz)%max_sz]){
+                ++total;
+            }
+            if(cells[(i-1+max_sz)%max_sz][(j+1+max_sz)%max_sz]){
+                ++total;
+            }
+            if(cells[(i+max_sz)%max_sz][(j-1+max_sz)%max_sz]){
+                ++total;
+            }
+            if(cells[(i+max_sz)%max_sz][(j+1+max_sz)%max_sz]){
+                ++total;
+            }
+            if(cells[(i+1+max_sz)%max_sz][(j-1+max_sz)%max_sz]){
+                ++total;
+            }
+            if(cells[(i+1+max_sz)%max_sz][(j+max_sz)%max_sz]){
+                ++total;
+            }
+            if(cells[(i+1+max_sz)%max_sz][(j+1+max_sz)%max_sz]){
+                ++total;
+            }
+
+            if(cells[i][j]){ //if the current cell is living
+                if((total != 2) && (total != 3)){ //apply the rules
+                    cells[i][j] = 0;
+                }
+            } else{ //the current cell is nonliving
+                if(total == 3){ //apply the rules
+                    cells[i][j] = 1;
+                }
+            }
+        }
+    }
+}
+
+void life::paintEvent(QPaintEvent *event){
+    QPainter paint(this);
+    for(size_t i = 0; i < max_sz; ++i){ //iterate over every cell
+        for(size_t j = 0; j < max_sz; ++j){
+            QRectF cell(getCoord(i), getCoord(j), cell_sz, cell_sz); //calculate the rectangle to fill for the cell
+            if(cells[i][j]){ //if the cell is living
+                paint.fillRect(r, QBrush(255, 191, 0)); //r:255 g:191 b:0 gives amber
+            }
+            else{ //the cell is not living
+                paint.fillRect(r, QBrush(136, 48, 0)); //r:136 g:48 b:0 gives dark amber
+            }
+        }
+    }
+}
+
+size_t life::getIndex(int c){
+    return (size_t) c / cell_sz; //compute the index corresponding to the given coordinate
+}
+
+int life::getCoord(size_t i){
+    return (int) i * cell_sz; //compute the screen coordinate for the given index
 }
 
 life::~life()
--- life.h
+++ life.h
@@ -3,6 +3,7 @@
 
 #include <QWidget>
 #include <QPaintEvent>
+#include <ctime>
 
 namespace Ui {
 class life;
@@ -17,22 +18,25 @@ public:
     ~life();
 
 public slots:
-    void nextGeneration(); //compute the next generation of the game
     void clear(); //clear the game world
     void randomize(); //randomize the game world
+    void nextGeneration(); //compute the next generation of the game
 
 protected:
     void paintEvent(QPaintEvent *event); //the function for drawing the game
 
 private:
     Ui::life *ui;
-    static const size_t grid_sz = 50; //the max size of the widget
-    static const size_t cell_sz = 10; //the size of a cell
+    static const size_t max_sz = 100; //the max size of the widget
+    static const size_t min_sz = 10; //the min size of the widget
+    static const size_t cell_sz = 10; //the size of a cell when it is drawn
+    //size_t border_x = max_sz; //to support resizing the window, we will keep track of the edges of the
+    //size_t border_y = max_sz; //portion of the world being displayed
 
-    bool cells[grid_sz][grid_sz]; //the game world
+    bool cells[max_sz][max_sz]; //the game world
 
     size_t getIndex(int c); //will take a coordinate and give the corresponding index of cells[][]
-    int getCoord(int i); //will take an index of cells[][] and give the corresponding coordinate for drawing
+    int getCoord(size_t i); //will take an index of cells[][] and give the corresponding coordinate for drawing
 };
 
 #endif // LIFE_H
